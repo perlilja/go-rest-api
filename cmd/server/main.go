5cb76daf98f07fc27dea8e3fa5ebf8ab072448d9
Per Lilja <perlilja@gmail.com>
2022-01-27 15:44:06 +0100

    Structuring our application

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up our APP.")
+	return nil
+}
+
+func main() {
+	fmt.Println("Go REST API Course")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pelilja/go-rest-api
+
+go 1.17
